8788520f94ed5345a91bcdd3835b1c2c80ab3db4
soren beech <soren.beech@gmail.com>
2019-07-25 14:42:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
-var conversation = "Let's talk about space";
-
-var conversation = `${topic}`;
+var conversation = "Let's talk about `${topic}`";
